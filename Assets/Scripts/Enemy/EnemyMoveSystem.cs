92cb48076d6cb7b7e75550c1b1e3dd4e67594d5a
Ita-lack <ieasouzagit@gmail.com>
2025-07-05 14:46:27 -0300

    pelo menos se move até o player

--- Assets/Scripts/Enemy/EnemyMoveSystem.cs
+++ Assets/Scripts/Enemy/EnemyMoveSystem.cs
@@ -2,14 +2,17 @@ using Unity.Entities;
 using Unity.Burst;
 using Unity.Transforms;
 using Unity.Mathematics;
+using UnityEngine;
 
 public partial struct EnemyMoveSystem : ISystem {
     [BurstCompile]
     public void OnUpdate(ref SystemState state){
         Entity playerEntity;
-        LocalTransform playerTransform;
-        if (!SystemAPI.TryGetSingletonEntity<PlayerTag>(out playerEntity)) return;
-        if (!SystemAPI.TryGetSingleton(out playerTransform)) return;
+        Debug.Log("cheguasasasasi aqui?");
+        if (!SystemAPI.TryGetSingletonEntity<playerTag>(out playerEntity)) return;
+        Debug.Log("cheguei aqui?");
+        LocalTransform playerTransform = SystemAPI.GetComponent<LocalTransform>(playerEntity);
+        Debug.Log("passei aqui?");
         float deltaTime = SystemAPI.Time.DeltaTime;
         foreach (var (transform, moveSpeed) in SystemAPI.Query<RefRW<LocalTransform>, RefRO<MoveSpeedEnemy>>().WithAll<EnemyTag>()){
             var direction = math.normalize(playerTransform.Position - transform.ValueRO.Position);
